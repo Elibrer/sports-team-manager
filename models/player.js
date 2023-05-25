8aeed92ef195e7d1b0db50a7ee19232f209077ed
Your Name <you@example.com>
2023-05-25 20:01:09 +1000

    Modules added

--- models/player.js
+++ models/player.js
@@ -1,2 +1,66 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
+class Player extends Model {
+  hasScores() {
+    if (this.scores > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  hasFouls() {
+    if (this.fouls > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
+Player.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    player_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    scores: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    fouls: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: 'player',
+    tableName: 'Player',
+  }
+);
+
+module.exports = Player;
