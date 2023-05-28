41bfd4481dde4e6fa35153f405175fa9ad1a97c2
Elibrer <eli.brer@gmail.com>
2023-05-28 18:21:31 +0930

    Updated /manage and /login pages

--- models/index.js
+++ models/index.js
@@ -2,18 +2,21 @@ const User = require('./User');
 const Position = require('./Position');
 const Player = require('./Player');
 
-Position.hasOne(Player, {
-    foreignKey: 'position_id',
-    onDelete: 'CASCADE',
-});
+// Player.belongsTo(Position, {   
+//     foreignKey: 'player_id',
+// });
 
-User.hasMany(Player, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
-});
+// Position.hasMany(Player, {
+//     foreignKey: 'position_id',
+// });
 
-Player.belongsTo(User, {
-    foreignKey: 'user_id',
-});
+// User.hasMany(Player, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE',
+// });
+
+// Player.belongsTo(User, {
+//     foreignKey: 'id',
+// });
 
 module.exports = { User, Position, Player };
--- models/player.js
+++ models/player.js
@@ -1,73 +1,72 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Player extends Model {
-  hasScores() {
-    if (this.scores > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  hasFouls() {
-    if (this.fouls > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
+// class Player extends Model {
+//   hasScores() {
+//     if (this.scores > 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   };
+//   hasFouls() {
+//     if (this.fouls > 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
 
-Player.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    first_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    last_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    player_number: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    scores: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    fouls: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-    position_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'position',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    underscored: true,
-    modelName: 'player',
-    tableName: 'Player',
-  }
-);
+// Player.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     first_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     last_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     player_number: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//     },
+//     scores: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+//     fouls: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+//     user_id: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: 'user',
+//         key: 'id',
+//       },
+//     },
+//     position_id: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: 'position',
+//         key: 'id',
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     underscored: true,
+//     modelName: 'player',
+//   }
+// );
 
-module.exports = Player;
+// module.exports = Player;
